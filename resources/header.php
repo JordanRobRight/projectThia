653e3f598b4b3a912d0d6f4c01057e7a13c2cf46
Michael Huang <huang65@uwm.edu>
2016-10-24 01:57:08 +0000

    header.php edited online with Bitbucket
    I included signup and login in the navbar for customers

--- resources/header.php
+++ resources/header.php
@@ -51,6 +51,10 @@
 						<li class="active"><a href="#">About</a></li>
 						<li class="active"><a href="#">Contact</a></li>
 					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+						<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
 		</nav>
