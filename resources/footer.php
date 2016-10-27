366a5163d505bc6a2adcaaaf65252fa932780ebc
Pauly Russ <paulydanyl@gmail.com>
2016-10-26 20:25:20 -0500

    update header/footer. Replace styles, move head content from footer to header, remove javascript/jquery links from header and into footer for faster page loads

--- resources/footer.php
+++ resources/footer.php
@@ -1,39 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-US">
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-		<title>Siam Thai</title>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<style>
-html {
-  position: relative;
-  min-height: 100%;
-}
-body {
-  /* Margin bottom by footer height */
-  margin-bottom: 60px;
-}
-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  /* Set the fixed height of the footer here */
-  height: 60px;
-  background-color: #f5f5f5;
-}
-    .text-muted{
-        text-align: center;
-        padding-top: 20px;
-    }
-</style>
-
-	</head>
 <body>
  <footer>
       <div class="container">
