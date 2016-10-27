ffd80ac110fd112886a8c5b2a6fbce3ab179e0d6
Pauly Russ <paulydanyl@gmail.com>
2016-10-26 22:19:24 -0500

    create draft of html dashboard

--- resources/header.php
+++ resources/header.php
@@ -1,64 +1,64 @@
 <!DOCTYPE html>
 <html lang="en-US">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <!-- changing this part can break compatibility and responsiveness in older browsers -->
-  
-  <title>Siam Thai</title>
-  
-  <!-- use free cdn to get fast page loads of latest compiled and minified css -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<style>
-		body {
-			padding-top: 50px; /*make space for bootstrap navbar*/
-			margin-bottom: 60px;  /* Margin bottom by footer height */
-		}
-		form {
-			margin: 10px; /*ensure form maintains margins on all screens*/
-		}
-		.btn {
-			margin-top: 15px;
-		}
-		footer {
-			position: absolute;
-			bottom: 0;
-			width: 100%;
-			height: 60px;  /* Set the fixed height of the footer here */
-			background-color: #f5f5f5;
-		}
-    .text-muted{
-        text-align: center;
-        padding-top: 20px;
-    }
-	</style>
-</head>
-<body>
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Siam Thai</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<!-- changing this part can break compatibility and responsiveness in older browsers -->
+		
+		<title>Siam Thai</title>
+		
+		<!-- use free cdn to get fast page loads of latest compiled and minified css -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<style>
+			body {
+				padding-top: 50px; /*make space for bootstrap navbar*/
+				margin-bottom: 60px;  /* Margin bottom by footer height */
+			}
+			form {
+				margin: 10px; /*ensure form maintains margins on all screens*/
+			}
+			.btn {
+				margin-top: 15px;
+			}
+			footer {
+				position: absolute;
+				bottom: 0;
+				width: 100%;
+				height: 60px;  /* Set the fixed height of the footer here */
+				background-color: #f5f5f5;
+			}
+			.text-muted{
+					text-align: center;
+					padding-top: 20px;
+			}
+		</style>
+	</head>
+	<body>
+	
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Siam Thai</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav">
+					<li><a href="#">Menu</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+					<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
 
