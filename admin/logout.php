bdda5c9261eb52f746983d9dfaf3137c1092240a
Pauly Russ <paulydanyl@gmail.com>
2016-12-14 23:15:35 -0600

    tweak logout

--- admin/logout.php
+++ admin/logout.php
@@ -1,7 +1,10 @@
 <?php
 session_start();
+# use php functions to unset and destroy session
 session_unset();
 session_destroy();
+# manually set PHPSESSID to NULL to remove session info from user's web browser
+setcookie('PHPSESSID', NULL);
 header('Location: index.php');
 exit;
 ?>
